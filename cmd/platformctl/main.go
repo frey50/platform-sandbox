4d37d115197c2611df16a42c0e6910f089c57ee2
Frey <freysair@Freys-MacBook-Air.local>
2026-08-18 17:33:23 +0500

    fix CI: CRD schemas, module layout, restore CLI entrypoint

--- cmd/platformctl/main.go
+++ cmd/platformctl/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/frey50/platform-sandbox/cmd/platformctl/cmd"
+)
 
 func main() {
-    fmt.Println("platformctl: Platform Sandbox CLI - Coming Soon")
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
 }
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/frey50/platform-sandbox
-
-go 1.26.4
